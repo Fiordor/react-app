4c12ad29fdf2c67ab237a24c4ccf8107f8273086
Fiordor <1fiordor@gmail.com>
2019-04-20 17:43:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { contador:0 }
+    this.state = { contador:0, x:0, y:0 }
+  }
+
+  _onMouseMove(e) {
+    this.setState({x: e.screenX, y: e.screenY })
   }
 
   aumentar() { this.setState({contador:this.state.contador+1}) }
@@ -17,11 +20,38 @@ class App extends Component {
   }
 
   render() {
+
+    const {x ,y} = this.state
+
+    const stylesOnX = {
+      position: "relative",
+      width: "5px",
+      height: "100%",
+      left: this.state.x - 343 + "px",
+      backgroundColor: "blue",
+    }
+
+    const stylesOnY = {
+      position: "relative",
+      width: "100%",
+      height: "5px",
+      top: this.state.y - 662 + "px",
+      backgroundColor: "blue",
+    }
+
     return (
-      <div>
-        <h3>{this.state.contador}</h3>
+      <div className="box" onMouseMove={this._onMouseMove.bind(this)}>
+
+        <div style={stylesOnX}></div>
+        <div style={stylesOnY}></div>
+
+        <h1>Mouse coordinates: {x} {y}</h1>
+
+        {/*
         <input type="button" onClick={this.aumentar.bind(this)} value="Incrementar"/>
+        <h3>{this.state.contador}</h3>
         <input type="button" onClick={this.decrementar.bind(this)} value="Decrementar"/>
+        */}
       </div>
     );
   }
